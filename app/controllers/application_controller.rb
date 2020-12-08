14a4bbf7cf8c2ec51eb931cc1c30a7163c42ac60
QueenTesa Fuggett <qfuggett@umich.edu>
2020-12-07 19:43:31 -0500

    create a scope method to show the most popular product

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,6 @@ class ApplicationController < ActionController::Base
     def welcome
         @routines = Routine.all
         @products = Product.all
+        @popular_product = Product.all.popular
     end
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
+  scope :popular, -> {group('name').order('count_all').limit(1).count}
   self.abstract_class = true
 end
